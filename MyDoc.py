6cf56ed357846153b6fceb4c492c73fad202e02b
K1DV5 <kidusadugna@gmail.com>
2018-06-10 21:21:53 +0300

    added units capability

--- MyDoc.py
+++ MyDoc.py
@@ -1,6 +1,7 @@
 from shutil import rmtree
 import sympy as sp
-from sympy import Matrix, sqrt, pi
+from sympy import Symbol
+from sympy.physics.units import meter, second, kilogram, convert_to
 import __main__
 class document:
 	'''contains (almost) all of the functionality for a LaTeX document'''
@@ -36,24 +37,33 @@ class document:
 			for eq in eqns:
 				self.append(eq.replace('=', '&=') +'\\\\', 1)
 			self.append('\\end{split}\n\\end{align}', 1)
-	def aserar(self, eqn, intent = 'full'):
+	def aserar(self, eqn, intent = 'full', unit = [meter, second, kilogram]):
+		def format_quantity(qty):
+			'''returns a nicely latex formatted string of the quantity including the units (if any)'''
+			try:
+				return(str(round(float(qty), 3)))
+			except:
+				number = list(convert_to(qty, unit).evalf(5).as_coefficients_dict().values())[0]
+				un = '\\mathrm{' + sp.latex(list(convert_to(qty.evalf(), unit).as_coefficients_dict().keys())[0]) + '}'
+				abb = {'meter': 'm', 'second': 's'}
+				fmtd = str(number) + '\\,' + un
+				for full_form, short_form in abb.items(): fmtd = fmtd.replace(full_form, short_form)
+				return fmtd
 		lhand = eqn.split('=')[0].strip()
 		simplified = sp.simplify(eqn.split('=')[1].strip())
 		variable_main = '__main__.' + lhand
-		free_symbols = list(simplified.free_symbols)
+		free_symbols = list(simplified.atoms(Symbol))
 		variables_list = [sp.latex(var) for var in free_symbols]
 		values_list = [eval('__main__.' + str(var)) for var in free_symbols]
+		values_dict = dict(zip(variables_list, values_list))
 		expr_2_str = str(simplified)
-		values_dict = dict()
-		for var in free_symbols:
-			expr_2_str = expr_2_str.replace(str(var), '__main__.'+str(var))
-			values_dict[var] = values_list[free_symbols.index(var)]
+		for var in free_symbols: expr_2_str = expr_2_str.replace(str(var), '__main__.'+str(var))
 		exec(variable_main + '=' + expr_2_str)
 		variable_lx = sp.latex(sp.simplify(lhand))
 		expr_1_lx = sp.latex(simplified, mul_symbol = 'dot')
 		expr_2_lx = sp.latex(simplified, mul_symbol = 'times')
-		for var in variables_list: expr_2_lx = expr_2_lx.replace(var, str(values_list[variables_list.index(var)]))
-		expr_3_lx = str(round(simplified.evalf(subs = values_dict), 3))
+		for var, val in values_dict.items(): expr_2_lx = expr_2_lx.replace(var, format_quantity(val))
+		expr_3_lx = format_quantity(simplified.subs(dict(zip(free_symbols, values_list))))
 		_tabs_ = len(variable_lx)//4
 		if intent == 'define':
 			self.equation (variable_lx + ' = ' + expr_3_lx, inline = True)
@@ -62,8 +72,8 @@ class document:
 		else:
 			self.equation (variable_lx + '\t= ' + expr_1_lx, _tabs_*'\t' + '\t= ' + expr_2_lx, _tabs_*'\t' + '\t= ' + expr_3_lx)
 	def __exit__(self, *args):
-		self.content = '\\documentclass[' + self.options + ']{' + self.type + '}\n\\usepackage{amsmath}' + self.preamble + '\n\\begin{document}' + self.content + '\n\n\\end{document}'
-		if self.dir == 'here': directory = '.'
+		self.content = '\\documentclass[' + self.options + ']{' + self.type + '}\n\\usepackage{amsmath}' + self.preamble + '\n\\begin{document}' + self.content.replace('_{}', '') + '\n\n\\end{document}'
+		if self.dir == 'here': directory = ''
 		else: directory = 'C:/Users/Kidus III/Documents/LaTeX/' + self.folder + '/'
 		with open(directory + self.name + '.tex', 'w') as file:
 			file.write(self.content)
--- example.py
+++ example.py
@@ -1,12 +1,10 @@
 from MyDoc import document
-from pint import UnitRegistry
-import numpy as np
-from sympy import eye, Matrix
-ur = UnitRegistry()
+from sympy.physics.units import meter, second, inch
 with document() as a:
 	a.options = '12pt'
 	a.chapter('''First Chapter''')
 	a.type = 'report'
+	a.file = 'docx'
 	a.append('''It should be emphasized that for systems in which the inputs are known ahead of time and in which there are no disturbances it is advisable to use open-loop control.  Closed-loop control systems have advantages only when unpredictable disturbanc and/or unpredictable variations in system components are present. Note that the  output power rating partially determines the cost,weight,and size of a control system.  The number of components used in a closed-loop control system is more than that for  a corresponding open-loop control system. Thus, the closed-loop control system is  generally higher in cost and power.To decrease the required power of a system,open-  loop control may be used where applicable.A proper combination of open-loop and  closed-loop controls is usually less expensive and will give satisfactory overall system  performance.''')
 	a.section('The section')
 
@@ -16,11 +14,9 @@ with document() as a:
 	a.append('''Note that the  output power rating partially determines the cost,weight,and size of a control system.  The number of components used in a closed-loop control system is more than that for  a corresponding open-loop control system. Thus, the closed-loop control system is  generally higher in cost and power.''')
 	a.equation('''L_M = \\pi\\times \\frac{\\theta}{56}''', inline=True)
 
-	# a.equation(r'x = \frac{\alpha+\beta}{\gamma}', ' = 56\\times x')
-	a.append('''Note that the  output power rating partially determines the cost,weight,and size of a control system.  The number of components used in a closed-loop control system is more than that for  a corresponding open-loop control system. Thus, the closed-loop control system is  generally higher in cost and power.''', 0)
-	beta_ = 3
-	sqrt = np.sqrt
-	Theta_ = 98# * ur.second
-	a.aserar('''B = sqrt(56*beta_/ pi)''')
+	a.append('''Note that the  output power rating partially determines the cost,weight,and size of a control system.  The number of components used in a closed-loop control system is more than that for  a corresponding open-loop control system. Thus, the closed-loop control system is  generally higher in cost and power.\\\\''', 0)
+	a.aserar('beta_ = 3*meter', 'define')
+	a.aserar('Theta_ = 98*inch', 'define')
+	a.aserar('''B = (beta_+Theta_)''', unit=inch)
 
 	a.append('''Note that the  output power rating partially determines the cost,weight,and size of a control system.  The number of components used in a closed-loop control system is more than that for  a corresponding open-loop control system. Thus, the closed-loop control system is generally higher in cost and power.''')
