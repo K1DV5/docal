594df96cc63e4276d7867d0e5b8c465e5de3d47c
K1DV5 <kidusadugna@gmail.com>
2018-11-23 17:19:20 +0300

    rewrote the module to remove class and sympy dep

--- docal/calculation.py
+++ docal/calculation.py
@@ -1,4 +1,3 @@
-# -ipy
 '''
 module procedure
 
@@ -12,82 +11,108 @@ from .equation import eqn
 from .formatting import format_quantity
 from .parsing import latexify
 
+DEFAULT_MAT_SIZE = 5
 
-class _calculation:
-    '''the calculation entity'''
+def _calculate(expr, mat_size):
+    '''carryout the necesary calculations and assignments'''
 
-    def __init__(self, equation):
-        '''prepare common variables for subsequent operations'''
+    expr_1_lx = latexify(expr, mul_symbol='.', mat_size=mat_size)
+    expr_2_lx = latexify(expr, mul_symbol='*', subs=True, mat_size=mat_size)
+    expr_3_lx = format_quantity(eval(expr, __dict__), mat_size)
 
-        self.main_variable = equation.split('=')[0].strip()
-        self.variable_lx = latexify(self.main_variable)
-        self.expr_0_str = equation.split('=')[1].strip()
+    return [expr_1_lx, expr_2_lx, expr_3_lx]
 
-    def step_one(self):
-        '''the first step of the procedure'''
+def figure_out_steps(expr_dump):
+    '''used when nothing about the steps comes from the user to prevent repetition
+    when the calculation is sent to the pdf. return necessary steps based on the
+    expression content.'''
 
-        expr_1_lx = latexify(self.expr_0_str, mul_symbol=' ')
-        # for full_form, short_form in UNIT_ABBREVIATIONS.items():
-        #     if full_form in expr_1_lx:
-        #         expr_1_lx = re.sub(
-        #             fr'(?<!_{{|[a-zA-Z_]{{2}}){full_form}(?![a-zA-Z_]+)',
-        #             f'\\mathrm{{{short_form}}}',
-        #             expr_1_lx)
-
-        return expr_1_lx
-
-    def step_two(self):
-        '''the second step'''
-
-        # free_symbols = list(self.sympified.atoms(Symbol))
-        # variables_list = [latex(var) for var in free_symbols]
-        # values_list = [__main__.__dict__[str(var)]
-        #                for var in free_symbols]
-        # values_dict = dict(zip(variables_list, values_list))
-        expr_2_lx = latexify(self.expr_0_str, mul_symbol='times', subs=True)
-        # for var, val in values_dict.items():
-        #     val_lx = format_quantity(val)
-        #     expr_2_lx = re.sub(
-        #         fr'(?<!_{{|[a-zA-Z_]{{2}}){re.escape(var)}\^?(?![a-zA-Z_]+)',
-        #         lambda mo: (fr'\left({val_lx}\right)^'
-        #                     if mo.group().endswith('^')
-        #                     and '\\mathrm' in val_lx
-        #                     else val_lx),
-        #         expr_2_lx)
-
-        return expr_2_lx
-
-    def step_three(self, shown_unit):
-        '''third step'''
-
-        expr_3_lx = format_quantity(eval(self.expr_0_str, __main__.__dict__),
-                                    shown_unit)
-
-        return expr_3_lx
-
-    def assign_variable(self, result_unit):
-        '''the name says it'''
-
-        if result_unit == (meter, kilogram, second):
-            main_value = self.expr_0_str
+    # there are no calls or ops in the expression
+    if not (any([typ in expr_dump for typ in ['Call', 'BinOp', 'UnaryOp']])):
+        if 'Name' in expr_dump:
+            steps = [0, 2]
         else:
-            try:
-                main_value = convert_to(eval(self.expr_0_str,
-                                             __main__.__dict__),
-                                        result_unit)
-            except:  # if it cannot be converted
-                main_value = self.expr_0_str
+            steps = [2]
+    else:
+        if 'Name' in expr_dump:
+            steps = [0, 1, 2]
+        else:
+            steps = [0, 2]
 
-        exec(f'{self.main_variable} = {main_value}', __main__.__dict__)
+    return steps
 
 
-def cal(equation, intent='full', unit=(meter, kilogram, second)):
+def steps_vs_mat_size(num_str, expr_str):
+    '''receive a number string and figure out which is supposed to be
+    configured between steps and matrix size. If the number is to configure
+    the matrix size, figure out which steps to take from the equation, else
+    take the default matrix size.'''
+
+    # the minimum matrix size is 4*4 and min array size is 4.
+    min_mat = 44
+    min_arr = 4
+    as_num = int(num_str)
+    if len(num_str) == 1:
+        if as_num > min_arr:
+            steps = figure_out_steps(expr_str)
+            mat_size = as_num
+        else:
+            steps = [as_num]
+            mat_size = DEFAULT_MAT_SIZE
+    else:
+        if as_num > min_mat:
+            steps = figure_out_steps(expr_str)
+            mat_size = (int(num_str[0]), int(num_str[1]))
+        else:
+            steps = [int(num) for num in num_str]
+            mat_size = DEFAULT_MAT_SIZE
+
+    return steps, mat_size
+
+
+def _assort_input(input_str):
+    '''look above'''
+
+    input_parts = [part.strip() for part in input_str.strip().split('#')]
+    if len(input_parts) == 1:
+        additionals = ''
+        equation = input_str
+    else:
+        equation = input_parts[0]
+        additionals = input_parts[1]
+
+    var_name, expression = [part.strip() for part in equation.split('=')]
+    expr_dump = ast.dump(ast.parse(expression))
+
+    if additionals.isdigit():
+        steps, mat_size = steps_vs_mat_size(additionals, expression)
+        unit = ''
+    elif ',' in additionals:
+        parts = [part.strip() for part in additionals.split(',')]
+        if not parts[0].isdigit():
+            parts.reverse()
+        steps, mat_size = steps_vs_mat_size(parts[0], expr_dump)
+        unit = parts[1]
+    else:
+        steps = figure_out_steps(expr_dump)
+        mat_size = DEFAULT_MAT_SIZE
+        unit = additionals
+
+    if unit:
+        if unit == 'deg':
+            unit = '^\\circ'
+        else:
+            unit = f" \, \mathrm{{{latexify(unit, mul_symbol=' ', div_symbol='/')}}}"
+
+    return var_name, expression, unit, steps, mat_size
+
+def cal(input_str):
     '''
     evaluate all the calculations, carry out the appropriate assignments,
     and return all the procedures
     (which can be inserted in a pweave or pythontex document with print())
 
-    >>> cal('t_f = 56', 'd')
+    >>> cal('t_f = 56 #1')
     $t_{f} = 56$
 
     >>> print(cal('r_d = sqrt(t_f/56)+78'))
@@ -101,36 +126,20 @@ def cal(equation, intent='full', unit=(meter, kilogram, second)):
     \\end{align}
     '''
 
-    calc = _calculation(equation)
-    calc.assign_variable(unit)
+    var_name, expr, unit, steps, mat_size = _assort_input(input_str)
+    result = _calculate(expr, mat_size)
+    var_lx = latexify(var_name)
+    result[2] += unit
 
-    _var_len = len(calc.variable_lx)
-    _rem = 4 - _var_len // 4
-    _spaces = _var_len + _rem
-
-    if intent in ('define', 'd'):
-        output = eqn(calc.variable_lx + _rem*' ' + '= ' + calc.step_one(),
-                     norm=False, disp=False)
-
-    elif intent in ('2step', '2'):
-        output = eqn(calc.variable_lx + _rem*' ' + '= ' + calc.step_one(),
-                     _spaces * ' ' + '= ' + calc.step_three(unit),
-                     norm=False, disp=True)
-
-    elif intent in ('last', 'l'):
-        output = eqn(calc.variable_lx + _rem*' ' + '= ' + calc.step_three(unit),
-                     norm=False, disp=True)
-
+    if len(steps) == 1 and steps[0] == 0 or steps[0] == 2:
+        displ = False
     else:
-        output = eqn(calc.variable_lx + _rem*' ' + '= ' + calc.step_one(),
-                     _spaces * ' ' + '= ' + calc.step_two(),
-                     _spaces * ' ' + '= ' + calc.step_three(unit),
-                     norm=False, disp=True)
+        displ = True
+
+    procedure = [f'{var_lx} = {result[steps[0]]}']
+    for step in [result[step] for step in steps[1:]]:
+        procedure.append('    = ' + step)
+
+    output = eqn(*procedure, norm=False, disp=displ)
 
     return output
-
-
-def fmt(quantity, unit=(meter, kilogram, second)):
-    '''when just formatting is needed'''
-
-    return _surround_equation(format_quantity(quantity, unit), disp=False)
