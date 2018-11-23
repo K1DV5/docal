fd11ff30b3d49246ad9aa5f9d012671b6129f585
K1DV5 <kidusadugna@gmail.com>
2018-11-23 17:29:30 +0300

    import fmt from formatting

--- docal/__init__.py
+++ docal/__init__.py
@@ -1,7 +1,8 @@
 '''initializiation module'''
 
 from .equation import eqn
-from .calculation import cal, fmt
+from .calculation import cal
+from .formatting import format_quantity as fmt
 from .document import document
 
 name = 'docal'
