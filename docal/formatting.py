08d6de3879a6c381065c36bfb8d7aad5d4f72890
K1DV5 <kidusadugna@gmail.com>
2018-11-23 17:25:17 +0300

    remove sympy dep and units formatting, fixed bugs

--- docal/formatting.py
+++ docal/formatting.py
@@ -1,62 +1,62 @@
 # -ipy
 
 import re
+from .parsing import latexify
 
 def _format_number(number):
     '''make the number part of a quantity more readable'''
 
-    if number > 1000 or number < 0.1:
+    if number != 0 and (number > 1000 or number < 0.1):
         # in scientific notation
-        number = re.sub(r'([0-9]+)E([-+])([0-9]+)',
+        return re.sub(r'([0-9]+)E([-+])([0-9]+)',
                         r'\1\\left(10^{\2'+r'\g<3>'.lstrip(r'0')+r'}\\right)',
                         f'{number:.2E}').replace('+', '')
-    else:
-        if number == int(number):
-            number = str(int(number))
-        else:
-            number = str(round(number, 3))
+    if number == int(number):
+        return str(int(number))
+    number = str(round(number, 3))
 
     return number
 
 
-def _format_array(array):
+def _format_array(array, max_size=5):
     '''look above'''
 
-    if len(array) > 4:
-        array = [*[_format_number(element) for element in array[:2]],
+    if len(array) > max_size:
+        array = [*[_format_number(element) for element in array[:max_size - 2]],
                  '\\vdots',
                  _format_number(array[-1])]
     else:
         array = [_format_number(element) for element in array]
 
-    number = ('\\left[\\begin{matrix}'
-              + '\\\\'.join(array)
-              + '\\end{matrix}\\right]')
+    number = ('\\left[\\begin{matrix}\n'
+              + '\\\\\n'.join(array)
+              + '\n\\end{matrix}\\right]')
 
     return f'{number}'
 
 
-def _format_big_matrix(matrix):
+def _format_big_matrix(matrix, size):
     '''look above'''
 
-    cut_matrix = matrix[:2, :2].tolist()
-    last_col = matrix[:2, -1].tolist()
-    last_row = matrix[-1, :2].tolist()
+    rows, cols = size
+    cut_matrix = matrix[:rows - 2, :cols - 2].tolist()
+    last_col = matrix[:rows - 2, -1].tolist()
+    last_row = matrix[-1, :cols - 2].tolist()
     last_element = matrix[-1,-1]
 
     mat_ls = [' & '.join(
         [_format_number(element) for element in cut_matrix[index]]) + ' & \\cdots & ' + _format_number(last_col[index][0])
         for index in range(len(cut_matrix))] \
-        + [' & '.join(['\\vdots'] * 2 + ['\\ddots', '\\vdots'])] \
+        + [' & '.join(['\\vdots'] * (cols - 2) + ['\\ddots', '\\vdots'])] \
         + [' & '.join([_format_number(element) for element in last_row[0]]) + ' & \\cdots & ' + _format_number(last_element)]
 
     return mat_ls
 
 
-def _format_wide_matrix(matrix):
+def _format_wide_matrix(matrix, max_cols):
     '''look above'''
 
-    cut_matrix = matrix[:, :2].tolist()
+    cut_matrix = matrix[:, :max_cols - 2].tolist()
     last_col = matrix[:, -1].tolist()
 
     mat_ls = [' & '.join(
@@ -65,54 +65,63 @@ def _format_wide_matrix(matrix):
 
     return mat_ls
 
-def _format_long_matrix(matrix):
+def _format_long_matrix(matrix, max_rows):
     '''look above'''
 
     mat_ls = [' & '.join(
-                          [_format_number(element) for element in matrix[:2, :].tolist()[index]])
-                          for index in range(matrix[:2, :].shape[0])]
+                          [_format_number(element) for element in matrix[:max_rows - 2, :].tolist()[index]])
+                          for index in range(matrix[:max_rows - 2, :].shape[0])]
     mat_ls.append(' & '.join(['\\vdots'] * matrix.shape[1]))
     mat_ls.append(' & '.join([_format_number(element) for element in matrix[-1, :].tolist()[0]]))
 
     return mat_ls
 
 
-def _format_matrix(matrix):
+def _format_matrix(matrix, max_size=(5,5)):
     '''look above'''
 
-    if matrix.shape[0] > 4 and matrix.shape[1] > 4:
-        mat_ls = _format_big_matrix(matrix)
-    elif matrix.shape[0] > 4 and matrix.shape[1] < 5:
-        mat_ls = _format_long_matrix(matrix)
-    elif matrix.shape[0] < 5 and matrix.shape[1] > 4:
-        mat_ls = _format_wide_matrix(matrix)
+    # too big -> small
+    if matrix.shape[0] > max_size[0] and matrix.shape[1] > max_size[1]:
+        mat_ls = _format_big_matrix(matrix, max_size)
+    # too long -> small
+    elif matrix.shape[0] > max_size[0] and matrix.shape[1] < max_size[1]:
+        mat_ls = _format_long_matrix(matrix, max_size[0])
+    # too wide -> small
+    elif matrix.shape[0] < max_size[0] and matrix.shape[1] > max_size[1]:
+        mat_ls = _format_wide_matrix(matrix, max_size[1])
+    # already small so :)
     else:
         mat_ls = [' & '.join(
             [_format_number(element) for element in matrix.tolist()[index]])
             for index in range(matrix.shape[0])]
 
-    number = ('\\left[\\begin{matrix}' +
-              '\\\\'.join(mat_ls) +
-              '\\end{matrix}\\right]')
+    return ('\\left[\\begin{matrix}\n' +
+              '\\\\\n'.join(mat_ls) +
+              '\n\\end{matrix}\\right]')
 
 
-    return number
+def format_quantity(quantity, mat_size=(5,5)):
+    '''returns a nicely latex formatted string of the quantity'''
 
-def format_quantity(quantity):
-    '''returns a nicely latex formatted string of the quantity
-    including the units (if any)'''
+    if isinstance(mat_size, int):
+        size_mat = (mat_size, mat_size)
+        size_arr = mat_size
+    else:
+        size_mat = mat_size
+        size_arr = mat_size[0]
 
     quantity_type = str(type(quantity))
 
-    if 'array' in quantity_type or 'Array' in quantity_type:
-        formatted = _format_array(quantity)
+    if any([isinstance(quantity, typ) for typ in [float, int]]):
+        formatted = _format_number(quantity)
+
+    elif 'array' in quantity_type or 'Array' in quantity_type:
+        formatted = _format_array(quantity, size_arr)
 
     elif 'matrix' in quantity_type:
-        formatted = _format_matrix(quantity)
+        formatted = _format_matrix(quantity, size_mat)
 
     else:
-        number = _format_number(quantity)
-        formatted = f'{number}'
+        formatted = str(quantity)
 
     return formatted
-
