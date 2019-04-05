1855458d1438d5b065ae41cb1670dad52e31a311
K1DV5 <kidusadugna@gmail.com>
2019-04-05 17:37:12 +0300

    cli: add input validators for the arguments

--- docal/__main__.py
+++ docal/__main__.py
@@ -7,25 +7,38 @@ from glob import glob
 from docal import document
 
 
+def calculation_file(arg: str) -> str:
+    'check if the argument is a path to a python script'
+    if arg.endswith('.py'):
+        return arg
+    raise ValueError("The calculation file name must end with '.py'.")
+
+
+def document_file(arg: str) -> str:
+    'same as above for documents'
+    if arg.endswith('.docx') or arg.endswith('.tex'):
+        return arg
+    raise ValueError("The document names must end with '.docx' or '.tex'.")
+
+
 # command line arguments
 parser = ArgumentParser(description="Process the script file, inject it to "
                         "the input document and produce the output document")
-parser.add_argument('script', help='The calculation file/script')
-parser.add_argument('-i', '--input', help='The document file to be modified')
-parser.add_argument('-o', '--output', help='The destination document file')
-parser.add_argument('-io', '--input-output',
-                    help="The document file to be modified in place. Use this "
-                    "when you don't want to create another file.")
+parser.add_argument(
+    'script', help='The calculation file/script', type=calculation_file)
+parser.add_argument(
+    '-i', '--input', help='The document file to be modified', type=document_file)
+parser.add_argument(
+    '-o', '--output', help='The destination document file', type=document_file)
 parser.add_argument('-c', '--clear', action='store_true',
                     help='Clear the calculations and try to '
                     'revert the document to the previous state. '
                     'Only for the calculation ranges in LaTeX files.')
 parser.add_argument('-l', '--log-level', choices=['INFO', 'WARNING', 'ERROR'],
-        help='How much info you want to see')
-args = parser.parse_args()
+                    help='How much info you want to see')
 
-if args.input_output:
-    args.input = args.output = args.input_output
+
+args = parser.parse_args()
 
 
 def main():
@@ -34,7 +47,7 @@ def main():
     '''
     try:
         if args.script:
-            with open(args.script) as file:
+            with open(args.script, encoding='utf-8') as file:
                 instructions = file.read()
         else:
             instructions = None
