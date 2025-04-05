380834ca5329aee1d0bb5f90970f639f76babaac
K1DV5 <kidusadugna@gmail.com>
2025-04-06 00:09:10 +0300

    improve lsp server to show only necessary info

--- docal/lsp.py
+++ docal/lsp.py
@@ -1,4 +1,5 @@
-from ast import Assign, Name, parse, unparse
+from ast import Assign, Constant, Name, Starred, Tuple, parse, unparse
+from typing import List
 from pygls.server import LanguageServer
 from lsprotocol.types import (
     InitializeParams,
@@ -26,6 +27,19 @@ def on_initialize(ls: DocalLSP, params: InitializeParams):
         }
     }
 
+def find_name_targets(target) -> list[Name]:
+    targets = []
+    if type(target) is Tuple or type(target) is List:
+        for elem in target.elts:
+            targets += find_name_targets(elem)
+    elif type(target) is Name:
+        targets.append(target)
+    elif isinstance(target, Starred):
+        targets += find_name_targets(target.value)
+    else:
+        raise TypeError('Unknown target', target)
+    return targets
+
 @server.feature('textDocument/didOpen')
 @server.feature('textDocument/didChange')
 def on_change(ls: DocalLSP, params):
@@ -35,18 +49,30 @@ def on_change(ls: DocalLSP, params):
     diagnostics = []
 
     globals = {}
-    for part in parse(text).body:
-        exec(unparse(part), globals=globals)
-        if type(part) != Assign:
+    try:
+        tree = parse(text)
+    except SyntaxError:
+        return
+    for part in tree.body:
+        try:
+            exec(unparse(part), globals=globals)
+        except:
+            break
+        if type(part) is not Assign:
             continue
-        target: Name
+        targets: list[Name] = []
         for target in part.targets:
+            targets += find_name_targets(target)
+        len_targets = len(targets)
+        if len_targets == 1 and type(part.value) is Constant:
+            continue
+        for target in targets:
             diagnostics.append(Diagnostic(
                 range=Range(
                     start=Position(line=target.lineno-1, character=target.col_offset),
                     end=Position(line=target.lineno-1, character=target.end_col_offset),
                 ),
-                message=f'{target.id} = {globals[target.id]}',
+                message=f'{target.id if len_targets > 1 else ""} = {globals[target.id]}'.strip(),
                 severity=DiagnosticSeverity.Information,
                 source=servername,
             ))
